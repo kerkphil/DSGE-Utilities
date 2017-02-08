55951223943bde98f7001b6a784fdb12a465b5d0
Kerk L. Phillips <kerkphil@gmail.com>
2017-02-07 21:36:17 -0700

    Changes to AKsolver - 'conv' changed to 'damp'

--- Auerbach-Kotlikoff Solver/AKsolve.py
+++ Auerbach-Kotlikoff Solver/AKsolve.py
@@ -2,15 +2,13 @@
 """
 Created on Sat Jan 28 13:31:47 2017
 
-@author: Kerk Phillips
+@author: Kerk
 """
 import numpy as np
 
-def AKsolve(Xguess, funcname, fparams, ccrit, conv, maxiter, shrinkon, \
+def AKsolve(Xguess, funcname, fparams, ccrit, damp, maxiter, shrinkon, \
     shrink, expandon, expand, disttype, display):
     '''
-    Version 1.0 January 2017
-    
     This function performs the Auerbach-Kotlikoff contraction mapping on a 
     function.
     
@@ -23,15 +21,15 @@ def AKsolve(Xguess, funcname, fparams, ccrit, conv, maxiter, shrinkon, \
         fparams:  A list of parameters used by funcname
         ccrit:  The value for distance between Xvalue and Xnew that indicates
             convergence to the fixed point
-        conv:  The weight put on Xnew relative to Xvalue when moving to the
-            next iteration; Xvalue = conv*Xnew + (1-conv)*Xvalue.
+        damp:  The weight put on Xnew relative to Xvalue when moving to the
+            next iteration; Xvalue = damp*Xnew + (1-damp)*Xvalue.
         maxiter:  The maximum number of iterations allowed
-        shrinkon:  If true, the value of conv is scaled down when the distance
+        shrinkon:  If true, the value of damp is scaled down when the distance
             between values of X in an iteration increases.
-        shrink:  The factor by which conv shrinks.
-        expandon:  If true, the value of conv is scaled up when the distance
+        shrink:  The factor by which damp shrinks.
+        expandon:  If true, the value of damp is scaled up when the distance
             between values of X in an iteration does not increase.
-        expand:  The factor by which conv expands.
+        expand:  The factor by which damp expands.
         disttype:  Indicator variable for the method used to compute distance
             between Xvalue and Xnew
             1: root mean squared differences  (default)
@@ -62,27 +60,27 @@ def AKsolve(Xguess, funcname, fparams, ccrit, conv, maxiter, shrinkon, \
             dist = np.amax(np.absolute(diff))
         else:
             dist = (np.mean(diff**2))**.5
-        # check if dist is falling, if not lower value of conv
+        # check if dist is falling, if not lower value of damp
         if (dist > distold) and (shrinkon):
-            # shrink conv and redo with same Xvalue, do not update count
-            conv = conv * shrink
-            Xvalue = conv*Xnew + (1-conv)*Xvalue
+            # shrink damp and redo with same Xvalue, do not update count
+            damp = damp * shrink
+            Xvalue = damp*Xnew + (1-damp)*Xvalue
             distold = dist
         else:
             # update Xvalue and count
             count = count + 1
             if expandon:
-                # expand conv if it is < 1.0
-                if conv < 1.0:
-                    conv = conv * 1.01
+                # expand damp if it is < 1.0
+                if damp < 1.0:
+                    damp = damp * 1.01
                 else:
-                    conv = 1.0
+                    damp = 1.0
             # take convex combination for new guess
-            Xvalue = conv*Xnew + (1-conv)*Xvalue
+            Xvalue = damp*Xnew + (1-damp)*Xvalue
             # replace old dist value
             distold = dist
         # show progress
         if display:
-            print "count: ", count, "distance: ", dist, "conv: ", conv
+            print "count: ", count, "distance: ", dist, "damp: ", damp
     
     return Xvalue, dist, count
